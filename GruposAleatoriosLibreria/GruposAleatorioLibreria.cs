6fc0c246f9fe7e4616555e1f32786311fc7a918c
sstephenmt <72407248@continental.edu.pe>
2020-12-02 08:59:11 -0500

    Libreria Randomizar

--- GruposAleatoriosLibreria/GruposAleatorioLibreria.cs
+++ GruposAleatoriosLibreria/GruposAleatorioLibreria.cs
@@ -8,5 +8,24 @@ namespace GruposAleatoriosLibreria
 {
     public class GruposAleatorioLibreria
     {
+        /// <summary>
+        ///  metodo para cambiar el orden en la cadena <br />
+        /// </summary>
+        public class Randomizer
+        {
+            public static void Randomize<T>(T[] items)
+            {
+                Random rand = new Random();
+                //desordena el array de forma aleatoria
+                for (int i = 0; i < items.Length - 1; i++)
+                {
+                    int j = rand.Next(i, items.Length);
+                    T temp = items[i];
+                    items[i] = items[j];
+                    items[j] = temp;
+                }
+            }
+        }
+
     }
 }
